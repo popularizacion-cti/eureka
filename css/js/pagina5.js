6c2d41962a1119c81787d1c123f429ed6bc18ca8
popularizacion-cti <popularizacion@concytec.gob.pe>
2026-05-25 09:09:47 -0500

    Add files via upload

---
+++ css/js/pagina5.js
@@ -0,0 +1,3 @@
+// Lógica exclusiva de la página 1
+console.log("El script de la página 1 se ha cargado correctamente.");
+// Aquí puedes programar juegos, formularios o lo que necesites para esta página
