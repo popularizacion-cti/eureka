9405efa245588b2bdb0db171627b018efd80d230
popularizacion-cti <popularizacion@concytec.gob.pe>
2026-05-25 09:10:19 -0500

    Delete css/js directory

--- css/js/inicio.js
+++
@@ -1,8 +0,0 @@
-// Lógica que solo funciona en la página de inicio
-const botonInicio = document.getElementById("boton-inicio");
-
-if (botonInicio) {
-    botonInicio.addEventListener("click", () => {
-        alert("¡Hola! Estás en la página principal. Este script es independiente.");
-    });
-}
--- css/js/pagina5.js
+++
@@ -1,3 +0,0 @@
-// Lógica exclusiva de la página 1
-console.log("El script de la página 1 se ha cargado correctamente.");
-// Aquí puedes programar juegos, formularios o lo que necesites para esta página
